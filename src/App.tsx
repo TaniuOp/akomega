9aa67544907e23e947c71192c5da40af52410cf1
TaniuBM <taniuop@gmail.com>
2024-04-21 16:29:12 +0200

    akomgea

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
+        <a href='https://www.traumheim-ag.ch/' target='_blank'>
           <img src={AKLogo} className='logo' alt='Vite logo' />
         </a>
       </div>
