12cf3a8d43c7a3e4511b1f5c91202556ee18da9c
TaniuBM <taniuop@gmail.com>
2024-04-21 16:22:18 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import AKLogo from '/akomegaag_logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={AKLogo} className='logo' alt='Vite logo' />
+        </a>
+      </div>
+      <h1>AK OMEGA AG</h1>
+      <div className='card'>
+        <p>Comming Soon</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
