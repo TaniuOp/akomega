24cd5f1989777b9f0e979e493afa4632b823e38b
TaniuBM <taniuop@gmail.com>
2024-04-21 20:04:17 +0200

    about us

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,6 +5,10 @@ import Services from './Services/Services';
 import Projects from './Projects/Projecsts';
 import Contact from './Contact/Contact';
 import Widen from '../../assets/widen.jpg';
+import Diana from '../../assets/diana.png';
+import Alex from '../../assets/alex.png';
+import Stefan from '../../assets/stefan.png';
+import Ivan from '../../assets/ivan.png';
 
 const Home = () => {
   useEffect(() => {
@@ -62,29 +66,84 @@ const Home = () => {
 
         <section className='team'>
           <h2>ÜBER UNS</h2>
-          <div className={'about-section'}>
-            <p className='about-us-texts'>
-              AK Omega AG ist ein Bauunternehmen mit mehr als 10 Jahren
-              Erfahrung, das sich auf den Wohnungsbau spezialisiert hat. Mit
-              Anerkennung im Kanton Aargau.
-            </p>
+          <div className='about-section'>
+            <div className='about-section-text'>
+              <p className='about-us-texts'>
+                AK Omega AG ist ein Bauunternehmen mit mehr als 10 Jahren
+                Erfahrung, das sich auf den Wohnungsbau spezialisiert hat. Mit
+                Anerkennung im Kanton Aargau.
+              </p>
 
-            <p className='about-us-texts'>
-              Das Team arbeitet unter der Leitung der Architekturabteilung, die
-              von der Architektin Diana Heger geführt wird. Der Leiter der
-              Bauabteilung ist Stefan Schüpfer. Die administrative und
-              immobilienwirtschaftliche Projektleitung liegt bei Ivan J.
-            </p>
-
-            <p className='about-us-texts'>
-              Darüber hinaus verfügen wir über ein professionelles Team von
-              Spezialisten in jedem Arbeitsbereich (Baumeister, Sanitär,
-              Elektro, Landschaftsbau...) für die Ausführung und ganzheitliche
-              Entwicklung unserer Projekte.
-            </p>
+              <p className='about-us-texts'>
+                Darüber hinaus verfügen wir über ein professionelles Team von
+                Spezialisten in jedem Arbeitsbereich (Baumeister, Sanitär,
+                Elektro, Landschaftsbau...) für die Ausführung und ganzheitliche
+                Entwicklung unserer Projekte.
+              </p>
+            </div>
           </div>
+
           <div className='about-us-extra'>
             <h3>Mehr als 27.000 Quadratmeter Bau entwickelt.</h3>
+          </div>
+
+          <div className='about-us-images-container'>
+            <div className='person-container'>
+              <img
+                src={Diana}
+                className='about-us-image'
+                alt='ak omega ag team'
+              />
+              <p className='person-name'>Diana Heger</p>
+              {/* <p className='person-description'>Projektleiter </p> */}
+              <p className='person-description'>Senior Architekt</p>
+            </div>
+
+            <div className='person-container'>
+              <img
+                src={Stefan}
+                className='about-us-image'
+                alt='ak omega ag team'
+              />
+              <p className='person-name'>Stefan S.</p>
+              <p className='person-description'>Bauherr</p>
+            </div>
+            <div className='person-container'>
+              <img
+                src={Ivan}
+                className='about-us-image'
+                alt='ak omega ag team'
+              />
+              <p className='person-name'>Ivan J.</p>
+              <p className='person-description'>Administration</p>
+            </div>
+            <div className='person-container'>
+              <img
+                src={Alex}
+                className='about-us-image'
+                alt='ak omega ag team'
+              />
+              <p className='person-name'>Alexandra G.</p>
+              <p className='person-nadescriptionme'>Architekt</p>
+            </div>
+            <div className='person-container'>
+              <img
+                src={Widen}
+                className='about-us-image'
+                alt='ak omega ag team'
+              />
+              <p className='person-name'>Ali</p>
+              <p className='person-description'>Bauleiter</p>
+            </div>
+            <div className='person-container'>
+              <img
+                src={Widen}
+                className='about-us-image'
+                alt='ak omega ag team'
+              />
+              <p className='person-name'>Roberto</p>
+              <p className='person-description'>Bauzeichnerin</p>
+            </div>
           </div>
         </section>
 
