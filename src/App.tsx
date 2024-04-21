7cb54fa0c52d1140dac9ad7d45e6e77e2665c519
TaniuBM <taniuop@gmail.com>
2024-04-21 18:36:36 +0200

    keys and main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import AKLogo from '/akomegaag_logo.png';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <>
-      <div>
-        <a href='https://www.traumheim-ag.ch/' target='_blank'>
-          <img src={AKLogo} className='logo' alt='AK logo' />
-        </a>
-      </div>
-      <h1>AK OMEGA AG</h1>
-      <div className='card'>
-        <p>Under construction</p>
+      <div className='main_container'>
+        <Header />
+        <Home />
+        <Footer />
       </div>
     </>
   );
---
+++ src/keys.ts
@@ -0,0 +1,2 @@
+export const googleMapsApiKey: string =
+  import.meta.env.VITE_GOOGLE_MAPS_API_KEY || '';
